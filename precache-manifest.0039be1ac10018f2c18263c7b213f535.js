ffdc74b879593776fa5192f2cda071070d0d4527
SalmaHa <salma.hamid008@gmail.com>
2021-10-04 14:36:19 +0300

    Updates

---
+++ precache-manifest.0039be1ac10018f2c18263c7b213f535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e93ad621305daf96634fffc01d4d7c",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "14cae744ac43992d9a45",
+    "url": "/TodoList/static/css/main.fa6cbe0a.chunk.css"
+  },
+  {
+    "revision": "1d32dd4a46a5df70a31b",
+    "url": "/TodoList/static/js/2.0bfb3b49.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TodoList/static/js/2.0bfb3b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14cae744ac43992d9a45",
+    "url": "/TodoList/static/js/main.80778ab2.chunk.js"
+  },
+  {
+    "revision": "15d80f514bcbf70e6f03",
+    "url": "/TodoList/static/js/runtime-main.79f12c4d.js"
+  }
+]);
